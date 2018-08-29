58b03a8397a57ae1397c4d46a24ac5466c9eb4c0
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2018-08-29 22:12:20 +0900

    create-shared_dir&_sidebar_file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,11 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  #deviseをrspecで使えるようにする
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
+  #FactoryGirlをrspecで使えるようにする
   config.include FactoryGirl::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
