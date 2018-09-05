e539156df8e6c2f0186e5ca9f58fb62c74e2fe3c
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2018-09-05 22:32:35 +0900

    create_message.js

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,49 @@
+$(function() {
+ function buildHTML(message) {
+    var img = message.image ? `<img class="lower-message__image" src=${ message.image }>`: "";
+    var html = `<div class="message__upper">
+                  <div class="message__upper__name">
+                    ${ message.user_name}
+                  </div>
+                  <div class="message__upper__date">
+                    ${ message.created_at}
+                  </div>
+                  <div class="message__lowwer">
+                    <p>${ message.content}</p>
+                       ${img}
+                  </div>
+                </div>`;
+    return html;
+  }
+  function scrollBottom(){  //メッセージの最下部へ自動スクロールする関数
+    var position = $(".chat__main")[0].scrollHeight;
+    $('.chat__main').animate({scrollTop: position}, 500);
+  }
+//イベント 発火設定
+  $('#message_form').on('submit', function(e){
+    e.preventDefault();//本来のsubmit処理をリセット
+    var formData = new FormData(this);  //フォームに入力された内容を取得
+    var url = $(this).attr('action');
+    //ajax処理
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    //ajax処理が成功した場合の処理
+    .done(function(message) {
+      var html = buildHTML(message);
+      $('.chat__main').append(html);  //入力された値を HTML反映
+      $('#message_form')[0].reset();  //入力された値をリセット
+      $('.form__submit-button').prop('disabled', false);  //submit処理を有効化
+      scrollBottom(); //自動スクロール
+    })
+    //ajax処理が失敗した場合の処理
+    .fail(function() {
+      alert('メッセージの送信に失敗しました');
+    })
+  });
+});
