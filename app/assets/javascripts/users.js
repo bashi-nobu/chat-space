9e28e7173acea214c4255354b5178de0eb863cd6
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2018-09-11 21:21:42 +0900

    delete-comment-out
    無駄なコメントアウトを消去

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -19,10 +19,8 @@ $(function() {
 
   //インクリメンタルサーチ処理
   $('#user-search-field').on('keyup', function(e){
-    e.preventDefault();//本来のsubmit処理をリセット
-    //フォームに入力された内容を取得
+    e.preventDefault();
     var inputData = $(this).val();
-    //ajax処理
     $.ajax({
       type: "GET",
       url: '/users',
@@ -33,7 +31,6 @@ $(function() {
     })
     //ajax処理が成功した場合の処理
     .done(function(data_list) {
-      //既存の検索結果をクリアにする
       $('#user-search-result').empty();
       //チャットメンバー欄に記載されているユーザーのidを取得し配列に保存
       var chat_member_list = [];
@@ -56,18 +53,15 @@ $(function() {
 
   //追加ボタンを押された際の処理
   $(document).on("click", ".user-search-add", function (e) {
-    var user_id = $(this).attr('data-user-id');   //追加ボタンを押されたユーザーのidを取得
-    var user_name = $(this).attr('data-user-name');  //追加ボタンを押されたユーザのnameを取得
-    //追加ボタンを押されたユーザーをチャットメンバー欄に追加
+    var user_id = $(this).attr('data-user-id');
+    var user_name = $(this).attr('data-user-name');
     var html = build_chatmember_HTML(user_id,user_name);
     $('#chat-group-users').append(html);
-    //検索候補欄から追加ボタンを押されたユーザーを除外
     $(this).parent().remove();
   });
 
   //削除ボタンを押された際の処理
   $(document).on("click", ".chat-group-user__btn--remove", function (e) {
-    //削除ボタンを押されたユーザーを除去
     $(this).parent().remove();
   });
 });
