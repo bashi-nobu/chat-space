ca346a19416cfb479209d4c2d41d73bc29464b52
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2018-09-13 23:36:53 +0900

    edit_message_controller
    datatypeがjson時の処理を追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,10 @@ class MessagesController < ApplicationController
     @group = Group.find(params[:group_id])
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def create
