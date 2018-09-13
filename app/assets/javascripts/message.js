ec9c17449db0eddc5556ce4b4bb73f76d16a18d5
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2018-09-13 23:38:17 +0900

    edit-message.js
    自動更新処理を追記

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
-$(function() {
- function buildHTML(message) {
+$(document).on('turbolinks:load', function() {
+  function buildHTML(message) {
     var img = message.image ? `<img class="lower-message__image" src=${ message.image }>`: "";
-    var html = `<div class="message__upper">
+    var html = `<div class="message__upper" data-message-id="${message.id}">
                   <div class="message__upper__name">
                     ${ message.user_name}
                   </div>
@@ -19,12 +19,11 @@ $(function() {
     var position = $(".chat__main")[0].scrollHeight;
     $('.chat__main').animate({scrollTop: position}, 500);
   }
-//イベント 発火設定
+  //イベント 発火設定
   $('#message_form').on('submit', function(e){
     e.preventDefault();//本来のsubmit処理をリセット
     var formData = new FormData(this);  //フォームに入力された内容を取得
     var url = $(this).attr('action');
-    //ajax処理
     $.ajax({
       type: "POST",
       url: url,
@@ -46,4 +45,31 @@ $(function() {
       alert('メッセージの送信に失敗しました');
     })
   });
+
+  //自動更新処理
+  var interval = setInterval(function(){
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      //チャットグループの最新のメッセージのidを取得
+      var last_message_id = $(".message__upper:last").attr("data-message-id");
+      $.ajax({
+        url: location.href.json,
+        dataType: 'json',
+      })
+      //ajax処理が成功した場合の処理
+      .done(function(message_list){
+        message_list.forEach(function(message){
+          if(message.id > last_message_id){
+            var html = buildHTML(message);
+            $('.chat__main').append(html);  //入力された値を HTML反映
+          }
+        });
+        scrollBottom(); //自動スクロール
+      })
+      //ajax処理が失敗した場合の処理
+      .fail(function() {
+        alert('メッセージの自動更新に失敗しました');
+      });
+    }else{
+    clearInterval(interval);
+  }}, 3*1000);
 });
